dc57490966686b3bd4b178bac306054b3e6930e5
David Baumgold <david@davidbaumgold.com>
2014-09-04 14:14:17 -0400

    Specify base repo

--- openedx_webhooks/__init__.py
+++ openedx_webhooks/__init__.py
@@ -143,7 +143,7 @@ def github_pull_request():
                 "description": pr["body"],
                 custom_fields["URL"]: pr["url"],
                 custom_fields["PR Number"]: pr["number"],
-                custom_fields["Repo"]: pr["repo"]["full_name"],
+                custom_fields["Repo"]: pr["base"]["repo"]["full_name"],
             }
         }
         resp = jira.post("/rest/api/2/issue", as_json=new_issue)
