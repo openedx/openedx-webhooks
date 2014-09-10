b4676ecac5b92fcad46f28dc0dda7e92d5827ede
David Baumgold <david@davidbaumgold.com>
2014-09-10 16:58:30 -0400

    Add ability to drop tables

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask.ext.script import Manager
+from flask.ext.script import Manager, prompt_bool
 from openedx_webhooks import app
 from openedx_webhooks.models import db
 
@@ -11,6 +11,14 @@ def dbcreate():
     "Creates database tables from SQLAlchemy models"
     db.create_all()
     db.session.commit()
+
+
+@manager.command
+def dbdrop():
+    "Drops database tables"
+    if prompt_bool("Are you sure you want to lose all your data"):
+        db.drop_all()
+        db.session.commit()
 
 
 if __name__ == "__main__":
