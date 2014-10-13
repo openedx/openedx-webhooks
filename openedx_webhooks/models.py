6878ddd0f07c94fc5314de61479ca2f9291db433
David Baumgold <david@davidbaumgold.com>
2014-10-13 15:26:03 -0400

    Flask-Dance 0.2.3

--- openedx_webhooks/models.py
+++ openedx_webhooks/models.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask_dance.models import OAuthMixin
+from flask_dance.models import OAuthConsumerMixin
 
 db = SQLAlchemy()
 
-class OAuth(db.Model, OAuthMixin):
+class OAuth(db.Model, OAuthConsumerMixin):
     pass
