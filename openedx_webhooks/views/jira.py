d92b6cbd05d0c9d1de225628af8af3c34474f1ee
David Baumgold <david@davidbaumgold.com>
2014-10-16 10:16:49 -0400

    author is under fields

--- openedx_webhooks/views/jira.py
+++ openedx_webhooks/views/jira.py
@@ -116,6 +116,9 @@ def jira_issue_created():
 
 def issue_opened(issue, bugsnag_context=None):
     bugsnag_context = bugsnag_context or {}
+    bugsnag_context = {"issue": issue}
+    bugsnag.configure_request(meta_data=bugsnag_context)
+
     issue_key = issue["key"].decode('utf-8')
     issue_status = issue["fields"]["status"]["name"].decode('utf-8')
     project = issue["fields"]["project"]["key"].decode('utf-8')
@@ -138,7 +141,7 @@ def issue_opened(issue, bugsnag_context=None):
         return "issue is OSPR"
 
     issue_url = URLObject(issue["self"])
-    user_url = URLObject(issue["author"]["self"])
+    user_url = URLObject(issue["fields"]["author"]["self"])
     user_url = user_url.set_query_param("expand", "groups")
 
     user_resp = jira_get(user_url)
@@ -174,9 +177,9 @@ def issue_opened(issue, bugsnag_context=None):
         transitioned = True
 
     try:
-        name = issue["author"]["displayName"].decode('utf-8')
+        name = issue["fields"]["author"]["displayName"].decode('utf-8')
     except:
-        bugsnag_context["name_type"] = type(issue["author"]["displayName"])
+        bugsnag_context["name_type"] = type(issue["fields"]["author"]["displayName"])
         bugsnag.configure_request(meta_data=bugsnag_context)
         raise
 
@@ -185,7 +188,7 @@ def issue_opened(issue, bugsnag_context=None):
         "{key} created by {name} ({username}), {action}".format(
             key=issue_key,
             name=name,
-            username=issue["author"]["name"].decode('utf-8'),
+            username=issue["fields"]["author"]["name"].decode('utf-8'),
             action="Transitioned to Open" if transitioned else "ignored",
         ),
         file=sys.stderr,
