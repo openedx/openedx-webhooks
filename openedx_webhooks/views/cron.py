1ef59808d00c9ea414658cc1961bfe26b3acb59d
David Baumgold <david@davidbaumgold.com>
2014-11-05 14:35:37 -0500

    Add group name, restructure daily cron

--- openedx_webhooks/views/cron.py
+++ openedx_webhooks/views/cron.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals, print_function
 import sys
 import json
 import bugsnag
+from collections import defaultdict
 from flask import make_response
 from flask_dance.contrib.jira import jira
 from openedx_webhooks import app
@@ -11,34 +12,34 @@ from openedx_webhooks.utils import jira_users, jira_group_members
 
 @app.route("/cron/daily", methods=("POST",))
 def cron_daily():
-    new_edx_employees = []
-    new_edx_employee_failures = {}
+    # a mapping of group name to email domain
+    domain_groups = {
+        "edx-employees": "@edx.org",
+    }
+    failures = defaultdict(dict)
 
-    edx_employee_users = jira_group_members("edx-employees", session=jira, debug=True)
-    edx_employee_usernames = set(u["name"] for u in edx_employee_users)
-    print(edx_employee_usernames, file=sys.stderr)
-    bugsnag_context = {"edx_employee_usernames": edx_employee_usernames}
-    bugsnag.configure_request(meta_data=bugsnag_context)
+    for groupname, domain in domain_groups.items():
+        users_in_group = jira_group_members("edx-employees", session=jira, debug=True)
+        usernames_in_group = set(u["name"] for u in users_in_group)
+        bugsnag_context = {
+            "groupname": groupname,
+            "usernames_in_group": usernames_in_group,
+        }
+        bugsnag.configure_request(meta_data=bugsnag_context)
 
-    # for all users with an "@edx.org" email address, put them in the
-    # edx-employees group
-    for user in jira_users(filter="@edx.org", session=jira, debug=True):
-        if not user["email"].endswith("@edx.org"):
-            pass
-        username = user["name"]
-        if username not in edx_employee_usernames:
-            # add the user to edx-employees!
-            resp = jira.post("/rest/api/2/group/user", json={"name": username})
-            if not resp.ok:
-                new_edx_employee_failures[username] = resp.text
-                bugsnag_context["new_edx_employee_failures"] = new_edx_employee_failures
-                bugsnag.configure_request(meta_data=bugsnag_context)
-            else:
-                new_edx_employees.append(username)
+        for user in jira_users(filter=domain, session=jira, debug=True):
+            if not user["email"].endswith(domain):
+                pass
+            username = user["name"]
+            if username not in usernames_in_group:
+                # add the user to the group!
+                resp = jira.post(
+                    "/rest/api/2/group/user",
+                    json={"name": username, "groupname": groupname},
+                )
+                if not resp.ok:
+                    failures[groupname][username] = resp.text
 
-    if new_edx_employee_failures:
-        resp = make_response(json.dumps(new_edx_employee_failures), 502)
-    else:
-        resp = make_response(json.dumps(new_edx_employees), 200)
+    resp = make_response(json.dumps(failures), 502 if failures else 200)
     resp.headers["Content-Type"] = "application/json"
     return resp
