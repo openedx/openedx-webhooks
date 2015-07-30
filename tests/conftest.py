0a1f4b63c9a3f7e9ae796421722a024f2cfa33c9
David Baumgold <david@davidbaumgold.com>
2015-07-30 13:38:27 -0400

    Don't record betamax cassettes when running on continuous integration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 import os
-import pytest
 import betamax
 
 if not os.path.exists('tests/cassettes'):
     os.makedirs('tests/cassettes')
 
+record_mode = 'none' if os.environ.get('CI') else 'once'
+
 with betamax.Betamax.configure() as config:
     config.cassette_library_dir = 'tests/cassettes/'
+    config.default_cassette_options['record_mode'] = record_mode
