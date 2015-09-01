d14f57161fcd47927b04fab372db032020e24978
David Baumgold <david@davidbaumgold.com>
2015-09-01 10:32:32 -0400

    Update test fixtures to use new private data repo

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import os
+import base64
 import pytest
 import betamax
+from requests_oauthlib import OAuth2Session
 import openedx_webhooks
 
 if not os.path.exists('tests/cassettes'):
@@ -11,6 +13,41 @@ record_mode = 'none' if os.environ.get('CI') else 'once'
 with betamax.Betamax.configure() as config:
     config.cassette_library_dir = 'tests/cassettes/'
     config.default_cassette_options['record_mode'] = record_mode
+    github_token = os.environ.get("GITHUB_TOKEN")
+    if github_token:
+        config.define_cassette_placeholder(
+            '<GITHUB-TOKEN>',
+            base64.b64encode(github_token.encode('utf-8'))
+        )
+        config.define_cassette_placeholder(
+            '<GITHUB-TOKEN>',
+            github_token.encode('utf-8')
+        )
+
+
+@pytest.fixture
+def github_session(request):
+    """
+    Like Betamax's built-in `betamax_session`, but with GitHub auth set up.
+    """
+    cassette_name = ''
+
+    if request.module is not None:
+        cassette_name += request.module.__name__ + '.'
+
+    if request.cls is not None:
+        cassette_name += request.cls.__name__ + '.'
+
+    cassette_name += request.function.__name__
+
+    github_token = os.environ.get("GITHUB_TOKEN")
+    token = {"access_token": github_token, "token_type": "bearer"}
+    session = OAuth2Session(token=token)
+    recorder = betamax.Betamax(session)
+    recorder.use_cassette(cassette_name)
+    recorder.start()
+    request.addfinalizer(recorder.stop)
+    return session
 
 
 @pytest.fixture
--- tests/test_pr_comments.py
+++ tests/test_pr_comments.py
@@ -5,7 +5,6 @@ from openedx_webhooks.github_views import (
     github_community_pr_comment, github_contractor_pr_comment
 )
 
-pytest.mark.usefixtures('betamax_session')
 
 def make_pull_request(user, number=1, base_repo_name="edx/edx-platform", head_repo_name="edx/edx-platform", created_at=None):
     "this should really use a framework like factory_boy"
@@ -34,15 +33,15 @@ def make_jira_issue(key="ABC-123"):
     }
 
 
-def test_community_pr_comment(app, betamax_session):
+def test_community_pr_comment(app, github_session):
     pr = make_pull_request(user="FakeUser")
     jira = make_jira_issue(key="FOO-1")
     with app.test_request_context('/'):
-        comment = github_community_pr_comment(pr, jira, session=betamax_session)
+        comment = github_community_pr_comment(pr, jira, session=github_session)
     assert "[FOO-1](https://openedx.atlassian.net/browse/FOO-1)" in comment
     assert "can't start reviewing your pull request" in comment
 
-def test_contractor_pr_comment(app, betamax_session):
+def test_contractor_pr_comment(app):
     pr = make_pull_request(user="FakeUser")
     ctx = app.test_request_context(
         '/', environ_overrides={"wsgi.url_scheme": "https"}
