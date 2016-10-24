a5469e5e1cb00ed921306e33f1d965af21c9a0f1
George Song <george@monozuku.com>
2016-10-24 10:14:19 -0700

    Add RQ worker to Procfile

---
+++ rq_worker.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Start an instance of the RQ worker.
+
+``python rq_worker.py``
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+
+from rq import Connection, Queue, Worker
+
+from openedx_webhooks.lib.rq import store
+
+
+LISTEN = ('default',)
+
+
+if __name__ == '__main__':
+    logging_level = os.environ.get('RQ_WORKER_LOGGING_LEVEL', 'INFO').upper()
+    with Connection(store):
+        worker = Worker(map(Queue, LISTEN))
+        worker.work(logging_level=logging_level)
