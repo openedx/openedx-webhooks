3cdc0b5948dfffaa0f3eec7120b149766f5d64e4
George Song <george@monozuku.com>
2017-01-03 13:45:40 -0800

    Update web hook installer to handle error cases

--- openedx_webhooks/lib/github/bin/update_or_create_webhooks.py
+++ openedx_webhooks/lib/github/bin/update_or_create_webhooks.py
@@ -6,17 +6,86 @@ Update or create webhooks according to spec.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import json
+from collections import defaultdict
 
 import click
+
 from openedx_webhooks.lib.github.utils import update_or_create_webhook
 
 click.disable_unicode_literals_warning = True
 
 
+class Results(object):
+    def __init__(self):
+        self.created = defaultdict(list)
+        self.edited = defaultdict(list)
+        self.deleted = defaultdict(list)
+        self.failed = defaultdict(list)
+
+    def add(self, repo, conf, hook, created, deleted):
+        url = conf['config']['url']
+        if hook:
+            if created:
+                self.created[url].append(repo)
+            else:
+                self.edited[url].append(repo)
+        else:
+            self.failed[url].append(repo)
+        if deleted:
+            self.deleted[url].append(repo)
+
+
+def install_hook(repo, conf, dry_run, verbose):
+    """
+    Install a webhook into specified repo.
+
+    Arguments:
+        repo (github3.repos.repo.Repository)
+        conf (dict): See description of "hooks" in cli docstring
+        dry_run (bool): Don't really edit the hook
+
+    Returns:
+        hook Optional[github3.repos.hook.Hook]: `None` if the install fails
+    """
+    active = conf.get('active', True)
+    config = conf['config']
+    events = conf['events']
+    url = config['url']
+
+    try:
+        hook, created, deleted = update_or_create_webhook(
+            repo, config, events, active, dry_run
+        )
+        if verbose or dry_run:
+            if created:
+                msg = "{} webhook created: {}".format(repo, url)
+            else:
+                msg = "{} webhook({}) updated: {}".format(repo, hook.id, url)
+            click.echo(msg)
+
+            for d in deleted:
+                msg = "{} webhook({}) deleted: {}".format(repo, d.id, url)
+                click.echo(msg)
+        return hook, created, deleted
+    except:
+        if verbose:
+            msg = "**FAILED** {} ({})".format(repo, url)
+            click.echo(msg)
+        return None, False, []
+
+
+def print_results(results_dict, header):
+    for url, repos in results_dict.items():
+        click.echo(header.format(url, len(repos)))
+        for repo in repos:
+            click.echo("* " + repo)
+
+
 @click.command()
 @click.argument('config-json', type=click.File('rb'))
 @click.option('--dry-run', is_flag=True)
-def cli(config_json, dry_run):
+@click.option('-v', '--verbose', is_flag=True)
+def cli(config_json, dry_run, verbose):
     """
     Update or create webhooks according to spec.
 
@@ -55,6 +124,8 @@ def cli(config_json, dry_run):
     repos = raw['repos']
     hook_confs = raw['hooks']
 
+    results = Results()
+
     if dry_run:
         click.echo(
             '**Dry run only** The following actions would have been performed:'
@@ -62,23 +133,25 @@ def cli(config_json, dry_run):
 
     for repo in repos:
         for conf in hook_confs:
-            active = conf.get('active', True)
-            config = conf['config']
-            events = conf['events']
-            url = config['url']
+            hook, created, deleted = install_hook(repo, conf, dry_run, verbose)
+            results.add(repo, conf, hook, created, deleted)
 
-            hook, created, deleted = update_or_create_webhook(
-                repo, config, events, active, dry_run
-            )
-            if created:
-                msg = "{} webhook created: {}".format(repo, url)
-            else:
-                msg = "{} webhook({}) updated: {}".format(repo, hook.id, url)
-            click.echo(msg)
+    print_results(
+        results.created, "{} webhook created for the following {} repos:"
+    )
 
-            for d in deleted:
-                msg = "{} webhook({}) deleted: {}".format(repo, d.id, url)
-                click.echo(msg)
+    print_results(
+        results.edited, "{} webhook edited for the following {} repos:"
+    )
+
+    print_results(
+        results.deleted, "{} webhook deleted for the following {} repos:"
+    )
+
+    print_results(
+        results.failed,
+        "{} webhook failed to install for the following {} repos:"
+    )
 
 
 if __name__ == '__main__':
