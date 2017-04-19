00f186cd159e05734fd3733f73e33bace80ab24c
George Song <george@monozuku.com>
2017-04-19 10:59:53 -0700

    Remove unnecessary import

--- openedx_webhooks/github/dispatcher/rules/github_activity.py
+++ openedx_webhooks/github/dispatcher/rules/github_activity.py
@@ -6,7 +6,6 @@ Update JIRA issue with latest GitHub activity.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from ....jira.tasks import update_latest_github_activity
-from ....lib.exceptions import NotFoundError
 from ....lib.github.decorators import inject_gh
 from ....lib.jira.decorators import inject_jira
 from ...models import GithubEvent
