2a5a1de804202237ffa6f779e14192b9e189a6ef
Ned Batchelder <ned@edx.org>
2020-02-28 12:37:26 -0500

    Use displayName, since name seems to no longer be provided?

--- openedx_webhooks/ui.py
+++ openedx_webhooks/ui.py
@@ -28,14 +28,14 @@ def index():
             except Exception as e:
                 logger.error("Failed to process response: {}".format(gh_user_resp.text))
                 raise
-                
+
 
     jira_username = None
     if jira_session.authorized:
         jira_user_resp = jira_session.get("/rest/api/2/myself")
         if jira_user_resp.ok:
             try:
-                jira_username = jira_user_resp.json()["name"]
+                jira_username = jira_user_resp.json()["displayName"]
             except Exception as e:
                 logger.error("Failed to process response: {}".format(jira_user_resp.text))
                 raise
