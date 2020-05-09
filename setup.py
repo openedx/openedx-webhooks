5f01ce7723f932d3f1813ba45aed8b7839f00847
Eric Herrera <eric.herrera@edunext.co>
2020-05-09 11:02:04 -0500

    Add tests for python 3.5 and python 3.8. Update classifiers.

--- setup.py
+++ setup.py
@@ -66,7 +66,8 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Framework :: Flask',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
     ),
     zip_safe=False,
 )
