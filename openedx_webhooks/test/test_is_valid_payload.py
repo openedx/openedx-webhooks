1391f6131b08267b798568b0d4727b875296d023
Ned Batchelder <ned@edx.org>
2020-05-31 06:48:55 -0400

    Simplify these tests

--- openedx_webhooks/test/test_is_valid_payload.py
+++ openedx_webhooks/test/test_is_valid_payload.py
@@ -1,3 +1,5 @@
+"""Tests of is_valid_payload."""
+
 import hmac
 import json
 from hashlib import sha1
@@ -8,45 +10,34 @@ from openedx_webhooks.utils import is_valid_payload
 
 
 def _make_signature(secret, payload):
+    """Compte a signature from a secret and a payload."""
     return (
         'sha1=' +
         hmac.new(secret.encode(), msg=payload, digestmod=sha1).hexdigest()
     )
 
 
-@pytest.fixture
-def secret1():
-    return 'top secret'
+SECRET1 = "top secret"
+SECRET2 = "not so top secret"
+PAYLOAD = json.dumps('payload').encode("utf8")
 
 
-@pytest.fixture
-def secret2():
-    return 'not so top secret'
+def test_everything_matches():
+    signature = _make_signature(SECRET1, PAYLOAD)
+    assert is_valid_payload(SECRET1, signature, PAYLOAD) is True
 
 
-@pytest.fixture
-def payload():
-    return json.dumps('payload').encode("utf8")
+def test_mismatched_signature():
+    wrong_signature = _make_signature(SECRET2, PAYLOAD)
+    assert is_valid_payload(SECRET1, wrong_signature, PAYLOAD) is False
 
 
-@pytest.fixture
-def signature(secret1, payload):
-    return _make_signature(secret1, payload)
+def test_bad_secret():
+    signature = _make_signature(SECRET1, PAYLOAD)
+    assert is_valid_payload(SECRET2, signature, PAYLOAD) is False
 
 
-def test_everything_matches(secret1, signature, payload):
-    assert is_valid_payload(secret1, signature, payload) is True
-
-
-def test_mismatched_signature(secret1, secret2, payload):
-    wrong_signature = _make_signature(secret2, payload)
-    assert is_valid_payload(secret1, wrong_signature, payload) is False
-
-
-def test_bad_secret(secret2, signature, payload):
-    assert is_valid_payload(secret2, signature, payload) is False
-
-
-def test_mismatched_payload(secret1, signature):
+def test_mismatched_payload():
+    signature = _make_signature(SECRET1, PAYLOAD)
     wrong_payload = json.dumps('x').encode("utf8")
-    assert is_valid_payload(secret1, signature, wrong_payload) is False
+    assert is_valid_payload(SECRET1, signature, wrong_payload) is False
