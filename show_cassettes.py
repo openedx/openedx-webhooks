26c9ff395edcefb94d2afbb85b64bb971c876351
Ned Batchelder <ned@edx.org>
2020-05-31 09:36:10 -0400

    Update show_cassettes.py for Python 3

--- show_cassettes.py
+++ show_cassettes.py
@@ -13,18 +13,18 @@ import sys
 for filename in sys.argv[1:]:
     with open(filename) as cassette:
         recorded_data = json.load(cassette)
-        print "{}:".format(filename)
+        print("{}:".format(filename))
         interactions = recorded_data['http_interactions']
         if interactions:
             for interaction in interactions:
-                print "    {}".format(interaction['request']['uri'])
+                print("    {}".format(interaction['request']['uri']))
                 status = interaction['response']['status']['code']
                 if status == 200:
                     for k, v in interaction['response']['body'].items():
                         if k in ['string', 'base64_string']:
                             v = "{} bytes".format(len(v))
-                        print "        {}: {}".format(k, v)
+                        print("        {}: {}".format(k, v))
                 else:
-                    print "        *** {}".format(status)
+                    print("        *** {}".format(status))
         else:
-            print "  - None -"
+            print("  - None -")
