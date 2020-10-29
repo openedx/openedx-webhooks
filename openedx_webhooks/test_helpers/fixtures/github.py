3592820f794c26382c992f16f7625a4500e0bfb0
Ned Batchelder <ned@edx.org>
2020-10-29 06:45:11 -0400

    Remove unused fixtures

--- openedx_webhooks/test_helpers/fixtures/github.py
+++ openedx_webhooks/test_helpers/fixtures/github.py
@@ -1,25 +1,4 @@
-from collections import namedtuple
-
 import pytest
-
-Hook = namedtuple('Hook', ['name', 'config', 'active'])
-Repo = namedtuple('Repo', ['hooks'])
-
-
-@pytest.fixture
-def hooks():
-    return [
-        Hook(name='web', config={'url': 'http://test.me'}, active=True),
-        Hook(name='travis', config={'url': 'http://travis'}, active=True),
-        Hook(name='web', config={'url': 'http://test.me'}, active=False),
-        Hook(name='trello', config={'url': 'http://test.me'}, active=True),
-        Hook(name='web', config={'url': 'http://test.inactive'}, active=False),
-    ]
-
-
-@pytest.fixture
-def repo(hooks):
-    return Repo(hooks=lambda: hooks)
 
 
 @pytest.fixture
