3ba2a8a6072857587269377635635afdc970b529
Ned Batchelder <ned@edx.org>
2021-09-15 17:20:53 -0400

    build: a commitlint GitHub Action

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+
+  helpUrl:
+    'https://open-edx-proposals.readthedocs.io/en/latest/oep-0051-bp-conventional-commits.html',
+
+  rules: {
+    'body-leading-blank': [0, "always"],
+
+    'subject-case': [0, "always", []],
+
+    'subject-full-stop': [0, "never", '.'],
+
+    'type-enum':
+      [2, 'always', ['revert', 'feat', 'fix', 'perf', 'docs', 'test', 'build', 'refactor', 'style', 'chore', 'temp']],
+  },
+};
