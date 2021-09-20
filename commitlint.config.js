42195e6aad53bdf624d932e1cc8a8c61a75fb2de
Ned Batchelder <ned@edx.org>
2021-09-20 15:23:17 -0400

    feat: allow revert messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
 
-  helpUrl:
-    'https://open-edx-proposals.readthedocs.io/en/latest/oep-0051-bp-conventional-commits.html',
+  helpUrl: 'https://open-edx-proposals.readthedocs.io/en/latest/oep-0051-bp-conventional-commits.html',
 
   rules: {
     'body-leading-blank': [0, "always"],
@@ -12,6 +11,15 @@ module.exports = {
     'subject-full-stop': [0, "never", '.'],
 
     'type-enum':
-      [2, 'always', ['revert', 'feat', 'fix', 'perf', 'docs', 'test', 'build', 'refactor', 'style', 'chore', 'temp']],
+      [2, 'always', [
+        'revert', 'feat', 'fix', 'perf', 'docs', 'test', 'build', 'refactor', 'style', 'chore', 'temp'
+      ]],
   },
+
+  ignores: [
+    // Allow GitHub revert messages, like:
+    //    Revert "introduce a bug"
+    //    Revert "introduce a bug" (#1234)
+    message => /^Revert ".*"( \(#\d+\))?/.test(message)
+  ],
 };
