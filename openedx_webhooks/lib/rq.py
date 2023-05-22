b60714f6b76ac418d5fae75da4dd67a510888e88
Feanil Patel <feanil@axim.org>
2023-05-22 10:41:33 -0400

    fix: Also don't verify certs for RQ.
    This is a bit of copy/paste from the openedx_webookhs/config.py but I
    thnk that's fine given that we're going to delete this soon.  I just
    don't want it to keep erroring until that happens in
    https://github.com/openedx/openedx-webhooks/pull/241

--- openedx_webhooks/lib/rq.py
+++ openedx_webhooks/lib/rq.py
@@ -9,6 +9,10 @@ from rq import Queue
 
 _redis_url = os.environ.get('REDIS_TLS_URL', os.environ.get('REDIS_URL', 'redis://'))
 
+redis_tls_options = "?ssl_cert_reqs=none"
+if _redis_url.startswith("rediss"):
+    _redis_url += redis_tls_options
+
 # redis.Redis: Instance of a connected Redis store
 store = redis.from_url(_redis_url)
 
