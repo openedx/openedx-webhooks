74487fc8430e2abd2a9da676f6490ba3dca395eb
Ned Batchelder <ned@edx.org>
2023-08-31 14:24:58 -0400

    refactor: GithubWebHookRequestHeader only did one tiny thing.

--- openedx_webhooks/lib/github/models.py
+++ openedx_webhooks/lib/github/models.py
@@ -8,38 +8,6 @@ import dataclasses
 
 from openedx_webhooks.types import PrDict
 
-class GithubWebHookRequestHeader:
-    """
-    Represent a GitHub webhook request header.
-
-    Attributes:
-        headers (flask.Request.headers): HTTP headers as received by Flask
-    """
-
-    def __init__(self, headers):
-        """
-        Init.
-
-        Arguments:
-            headers (flask.Request.headers): HTTP headers as received by Flask
-        """
-        self.headers = headers
-
-    @property
-    def event_type(self):
-        """
-        str: The webhook event type.
-        """
-        return self.headers.get('X-Github-Event')
-
-    @property
-    def signature(self):
-        """
-        str: Hash signature of the payload.
-        """
-        return self.headers.get('X-Hub-Signature')
-
-
 @dataclasses.dataclass(frozen=True)
 class PrId:
     """An id of a pull request, with three parts used by GitHub."""
