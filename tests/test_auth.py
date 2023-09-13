b09a39c576d86e48fdfb6edff9cf6b44cb1e98e0
Ned Batchelder <ned@edx.org>
2023-09-13 17:30:54 -0400

    refactor: test settings are now treated as environment variables

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,14 +1,15 @@
 import base64
 
 from openedx_webhooks.auth import get_github_session, get_jira_session
-from openedx_webhooks.settings import TestSettings
+
+from . import settings as test_settings
 
 
 def test_get_github_session(fake_github):
     session = get_github_session()
     response = session.get("/user")
     headers = response.request.headers
-    assert headers["Authorization"] == f"token {TestSettings.GITHUB_PERSONAL_TOKEN}"
+    assert headers["Authorization"] == f"token {test_settings.GITHUB_PERSONAL_TOKEN}"
     assert response.url == "https://api.github.com/user"
 
 
@@ -16,7 +17,7 @@ def test_get_jira_session(fake_jira):
     session = get_jira_session()
     response = session.get("/rest/api/2/field")
     headers = response.request.headers
-    user_token = f"{TestSettings.JIRA_USER_EMAIL}:{TestSettings.JIRA_USER_TOKEN}"
+    user_token = f"{test_settings.JIRA_USER_EMAIL}:{test_settings.JIRA_USER_TOKEN}"
     basic_auth = base64.b64encode(user_token.encode()).decode()
     assert headers["Authorization"] == f"Basic {basic_auth}"
-    assert response.url == f"{TestSettings.JIRA_SERVER}/rest/api/2/field"
+    assert response.url == f"{test_settings.JIRA_SERVER}/rest/api/2/field"
