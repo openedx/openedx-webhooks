7f7e8da75938cf40caacf819ee917dcf266300e2
Sarina Canelake <sarina@tcril.org>
2024-08-27 14:57:04 -0400

    feat!: Turn off survey link for merged/closed PRs (#299)

--- openedx_webhooks/tasks/pr_tracking.py
+++ openedx_webhooks/tasks/pr_tracking.py
@@ -291,8 +291,10 @@ def desired_support_state(pr: PrDict) -> PrDesiredInfo:
         if state == "reopened":
             desired.bot_comments_to_remove.add(BotComment.SURVEY)
 
-        if state in ["closed", "merged"]:
-            desired.bot_comments.add(BotComment.SURVEY)
+#        # temp: Disable survey link on pull requests
+#        # https://github.com/openedx/openedx-webhooks/issues/259
+#        if state in ["closed", "merged"]:
+#            desired.bot_comments.add(BotComment.SURVEY)
 
     if desired.is_refused and state not in ["closed", "merged"]:
         desired.bot_comments.add(BotComment.NO_CONTRIBUTIONS)
